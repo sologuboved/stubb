4daa129613097278a372b49d50c50d7c92b2fab9
sologuboved <sologuboved@gmail.com>
2017-12-16 21:43:23 +0300

    caught user_input <= 0

--- process_input_and_output.py
+++ process_input_and_output.py
@@ -3,6 +3,7 @@ from random_paragraph import *
 INVALID_INPUT = "Invalid input!"
 WRONG = "Something went wrong!"
 TOO_MUCH = 'There are as many as 136 Chapters in "Moby Dick", including the Epilogue'
+NOT_ZERO_ETC = 'The first chapter is chapter 1, named "Loomings"'
 
 
 def process_input_for_cet(user_input):
@@ -20,6 +21,9 @@ def process_input_for_cet(user_input):
 
     if last_num > LAST_NUM:
         return TOO_MUCH
+
+    if last_num < 1:
+        return NOT_ZERO_ETC
 
     try:
         title, paragraph = get_random_title_and_paragraph(last_num=last_num)
