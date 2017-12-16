aee710bd3f5dbc34a71d336b272c03c78bfbf9d6
sologuboved <sologuboved@gmail.com>
2017-12-16 14:12:59 +0300

    added get_random_title_and_paragraph and helper functions

---
+++ random_paragraph.py
@@ -0,0 +1,47 @@
+import random
+from json_operations import *
+
+EXTENSION = '.json'
+FOLDERNAME = 'mobydick_json/'
+TITLE = 'title'
+TEXT = 'text'
+LAST_NUM = 136
+
+
+def get_chapter(num):
+    try:
+        return load_json(FOLDERNAME + str(num) + EXTENSION)
+    except FileNotFoundError:
+        return
+
+
+def get_paragraph(text, ind):
+    try:
+        return text[ind]
+    except IndexError:
+        return
+
+
+def get_random_title_and_paragraph(last_num=LAST_NUM):
+    num = random.randint(1, last_num)
+    chapter = get_chapter(num)
+    if chapter is None:
+        return
+
+    title = chapter[TITLE]
+    text = chapter[TEXT]
+    ind = random.randrange(0, len(text))
+    paragraph = get_paragraph(text, ind)
+    if paragraph is None:
+        return
+
+    return title, paragraph
+
+
+if __name__ == '__main__':
+    tp = get_random_title_and_paragraph()
+    t, p = tp
+    print(t)
+    print()
+    print(p)
+
