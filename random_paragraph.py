27e4f3c182e39a090902737b73ed4dac576609ee
sologuboved <sologuboved@gmail.com>
2017-12-18 00:21:46 +0300

    style

--- random_paragraph.py
+++ random_paragraph.py
@@ -23,16 +23,16 @@ def get_paragraph(text, ind):
 
 
 def get_random_title_and_paragraph(last_num):
-    num = random.randint(1, last_num)
-    chapter = get_chapter(num)
+    random_num = random.randint(1, last_num)
+    chapter = get_chapter(random_num)
     if chapter is None:
         return
 
     title = chapter[TITLE]
     text = chapter[TEXT]
-    ind = random.randrange(0, len(text))
-    print("paragraph index:", ind, '\n')
-    paragraph = get_paragraph(text, ind)
+    random_ind = random.randrange(0, len(text))
+    print("paragraph index:", random_ind, '\n')
+    paragraph = get_paragraph(text, random_ind)
     if paragraph is None:
         return
 
