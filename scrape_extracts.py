b6bfeabf0204d2c1bdfeeb9732bfea4a1f4abf7a
sologuboved <sologuboved@gmail.com>
2017-12-17 15:28:33 +0300

    editing extracts

--- scrape_extracts.py
+++ scrape_extracts.py
@@ -35,11 +35,56 @@ def dump_raw_extracts():
     dump_json(raw_extracts, filename)
 
 
+def replace_raw_extract(ind, substitute):
+    filename = FOLDER + FILENAME_RAW + JSON
+    raw_extracts = load_json(filename)
+    raw_extracts[ind] = substitute
+    dump_json(raw_extracts, filename)
+
+
+def merge_raw_extracts(start_ind, end_ind):
+    filename = FOLDER + FILENAME_RAW + JSON
+    raw_extracts = load_json(filename)
+    to_merge = raw_extracts[start_ind: end_ind + 1]
+
+    merged = str()
+    for raw_extract in to_merge:
+        merged += '\n' + raw_extract
+
+    res = raw_extracts[: start_ind] + [merged] + raw_extracts[end_ind + 1:]
+
+    dump_json(res, filename)
+
+
+def omit_raw_extract(ind):
+    filename = FOLDER + FILENAME_RAW + JSON
+    raw_extracts = load_json(filename)
+    dump_json(raw_extracts[: ind] + raw_extracts[ind + 1:], filename)
+
+
+def prettyprint_raw_extracts():
+    raw_extracts = load_json(FOLDER + FILENAME_RAW + JSON)
+    ind = 0
+    for raw_extract in raw_extracts:
+        print(ind)
+        print(raw_extract)
+        print()
+        ind += 1
+
+
 if __name__ == '__main__':
     pass
-    r_e = load_json(FOLDER + FILENAME_RAW + JSON)
-    for e in r_e:
-        print(e)
-        print()
+
+    # omit_raw_extract(46)
+    # merge_raw_extracts(69, 70)
+
+    # replace_raw_extract(51, s51)
+
+    # print(s60)
+
+    prettyprint_raw_extracts()
+
+
+
 
 
---
+++ substitutes.py
@@ -0,0 +1,38 @@
+s25 = '"So close behind some promontory lie\n' \
+      'The huge Leviathans to attend their prey,\n' \
+      'And give no chace, but swallow in the fry,\n' \
+      'Which through their gaping jaws mistake the way"' \
+      '\nDryden\'s Annus Mirabilis.'
+
+s42 = '"Spain — a great whale stranded on the shores of Europe." Edmund Burke. (somewhere.)'
+
+s45 = '"Bright shone the roofs, the domes, the spires,\n' \
+      'And rockets blew self driven,\n' \
+      'To hang their momentary fire\n' \
+      'Around the vault of heaven.\n\n' \
+      'So fire with water to compare,\n' \
+      'The ocean serves on high,\n' \
+      'Up-spouted by a whale in air,\n' \
+      'To express unwieldy joy."\n' \
+      'Cowper, on the Queen\'s Visit to London.'
+
+s51 = '"Io! Paean! Io! sing,\n' \
+      'To the finny people\'s king.\n' \
+      'Not a mightier whale than this\n' \
+      'In the vast Atlantic is;\n' \
+      'Not a fatter fish than he,\n' \
+      'Flounders round the Polar Sea."\n' \
+      'Charles Lamb\'s Triumph of the Whale.'
+
+s55 = '"\'No, Sir, \'tis a Right Whale,\' answered Tom; \'I saw his spout; he threw up a pair of as pretty ' \
+      'rainbows as a Christian would wish to look at. He\'s a raal oil-butt, that fellow!\'"\nCooper\'s Pilot.'
+
+s57 = "\"'My God! Mr. Chace, what is the matter?' I answered, 'we have been stove by a whale.'\"\n" \
+      "\"Narrative of the Shipwreck of the Whale Ship Essex of Nantucket, which was attacked and Finally " \
+      "destroyed by a large Sperm Whale in the Pacific Ocean.\" By Owen Chace of Nantucket, first mate of said " \
+      "vessel. New York. 1821."
+
+s59 = '"The quantity of line withdrawn from the different boats engaged in the capture of this one whale, ' \
+      'amounted altogether to 10,440 yards or nearly six English miles. * * * Sometimes the whale shakes its ' \
+      'tremendous tail in the air, which, cracking like a whip, resounds to the distance of three or four ' \
+      'miles."\nScoresby.'
